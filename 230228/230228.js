6e7ff46d44425ec7b21fc60183068c9162152e9f
natreeum <projectkmscom@gmail.com>
2023-02-28 15:46:30 +0900

    230228

--- 230228/230228.js
+++ 230228/230228.js
@@ -1,1 +1,16 @@
-test;
+function solution(food) {
+  let front = [];
+  let back = [];
+  for (let i = 1; i < food.length; i++) {
+    if (food[i] % 2 == 1) food[i]--;
+    for (let j = 0; j < food[i] / 2; j++) {
+      front.push(i);
+      back.unshift(i);
+    }
+  }
+  var answer = `${front.join('')}0${back.join('')}`;
+  return answer;
+}
+
+const arr = [1, 10, 10, 10];
+console.log(solution(arr));
